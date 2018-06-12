85055d89358d58a79a72c4511064ca65a7f39b64
Jeremy Stern <jzstern@gmail.com>
2018-06-12 19:16:27 -0400

    home page is on App.js

---
+++ final-project/src/index.js
@@ -0,0 +1,16 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+
+// let store = createStore()
+
+import App from './App'
+
+ReactDOM.render(
+  <App />,
+	document.getElementById('root')
+)
+
+export default App
