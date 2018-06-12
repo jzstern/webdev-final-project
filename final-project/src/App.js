0f71fc5c8e5b23b2718bccf328e6ee9607b9024e
Jeremy Stern <jzstern@gmail.com>
2018-06-12 19:25:00 -0400

    sorry erin hahaha, making a new branch now

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import Header from './components/navigation-bar'
 
 class App extends Component {
 	render() {
 		return (
-			<div>
+			<div className="container-fluid">
+				<Header/>
 				<h1>hey guys asuhh</h1>
 			</div>
 		)
---
+++ final-project/src/components/navigation-bar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<div className="page-header">
+				<h1>This is the website header</h1>
+			</div>
+		)}
+
+	}
+export default Header
