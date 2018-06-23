b930e80f9580595e3d7402298a94bf44f518330c
erinzhangxi <erincheung1101@gmail.com>
2018-06-23 18:14:43 -0400

    added partial search

--- final-project/src/containers/Home.js
+++ final-project/src/containers/Home.js
@@ -6,17 +6,25 @@ import SongItem from '../components/song-item'
 export default class extends Component {
     constructor(props) {
         super(props);
-    this.renderResults = this.renderResults.bind(this);
+        this.state= {
+            results: []
+        }
+        this.renderResults = this.renderResults.bind(this);
     }
 
     componentDidMount() {
-
     }
+
+    // when songs are received, set to state then render
     renderResults(results) {
         console.log(results);
+        this.setState({results: results});
+    }
+
+    renderSongs() {
         let songs;
-        if (results !== null) {
-            songs = results.map((song) => {
+        if (this.state.results !== null) {
+            songs = this.state.results.map((song) => {
                 return <SongItem key={song._id}
                                  title={song.title}
                                  artist={song.artist}
@@ -29,6 +37,7 @@ export default class extends Component {
                                  description={song.description}/>
             });
         }
+
         return songs;
     }
 
@@ -36,8 +45,9 @@ export default class extends Component {
         return (
             <div className="container-fluid">
             <NavigationBar renderResults={this.renderResults}/>
-                <h3>Search Results</h3>
-                {this.renderResults}
+                <h3 hidden={this.state.results.length == 0}>Search Results</h3>
+                {this.renderSongs()}
+                <h3>Other people are listening</h3>
             </div>
         )
     }
