b530860d79fb031d4adaf6a62bc32d1f75e47a1c
Jeremy Stern <jzstern@gmail.com>
2018-06-27 17:32:24 -0400

    created playlist service on client side

---
+++ final-project/src/services/playlist.service.client.js
@@ -0,0 +1,65 @@
+let _singleton = Symbol()
+const URL = 'http://localhost:4000/api/playlist'
+
+class PlaylistService {
+	constructor(singletonToken) {
+		if (_singleton !== singletonToken)
+			throw new Error('Cannot instantiate directly.')
+	}
+
+	static get instance() {
+		if (!this[_singleton])
+			this[_singleton] = new PlaylistService(_singleton)
+		return this[_singleton]
+	}
+
+	findAllPlaylistsForUser(userId) {
+		return fetch(URL + '/user/' + userId)
+			.then(function(response) {
+				return response ? response.json() : {}
+			})
+	}
+
+	findPlaylistById(playlistId) {
+		return fetch(URL + '/' + playlistId)
+			.then(function(response) {
+				return response.json()
+			})
+	}
+
+	updatePlaylist(playlist) {
+		return fetch(URL + '/' + playlist._id, {
+			body: JSON.stringify(playlist),
+			headers: {
+				'content-type': 'application/json'
+			},
+			method: 'PUT'
+		})
+			.then(function(response) {
+				return response.json()
+			})
+	}
+
+	createPlaylistForUser(userId) {
+		return fetch(URL, {
+			body: JSON.stringify(userId),
+			headers: {
+				'content-type': 'application/json'
+			},
+			method: 'POST'
+		})
+			.then(function (response) {
+				return response.json()
+			})
+	}
+
+	deletePlaylist(playlistId) {
+		return fetch(URL + '/' + playlistId, {
+			method: 'DELETE'
+		})
+			.then(function(response) {
+				return response
+			})
+	}
+}
+export default PlaylistService
