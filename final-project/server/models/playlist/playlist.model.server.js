5e1dc4fc8a0927ac063586a3276ec9f4dedbc676
erinzhangxi <erincheung1101@gmail.com>
2018-06-27 17:55:32 -0400

    playlist backedn

--- final-project/server/models/playlist/playlist.model.server.js
+++ final-project/server/models/playlist/playlist.model.server.js
@@ -4,3 +4,34 @@ var playlistModel = mongoose.model('PlaylistModel', playlistSchema);
 var Schema = mongoose.Schema;
 
 
+function findAllPlaylistsForUser(userId) {
+    return playlistModel.find({"userId": userId});
+}
+
+function findPlaylistById(playlistId) {
+    return playlistModel.findById(playlistId);
+}
+
+function updatePlaylist(playlistId, playlist) {
+    return playlistModel.update({_id: playlistId},
+        {$set:playlistId});
+}
+
+function createPlaylistForUser(playlist) {
+    return playlistModel.create(playlist);
+}
+
+function deletePlaylist(playlistId) {
+    return playlistMode.deleteOne({_id: playlistId});
+}
+
+
+var api = {
+    findAllPlaylistsForUser: findAllPlaylistsForUser,
+    findPlaylistById: findPlaylistById,
+    updatePlaylist: updatePlaylist,
+    createPlaylistForUser: createPlaylistForUser,
+    deletePlaylist: deletePlaylist
+};
+
+module.exports = api;
